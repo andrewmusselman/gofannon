e3c7f14058a54dbb848f343361d7363bb84bd57a
Andrew Musselman <akm@apache.org>
2026-02-01 20:31:33 -0800

    Update playwright.config.js
    Fixing failing nightly worfklow
    
    Signed-off-by: Andrew Musselman <akm@apache.org>

--- webapp/playwright.config.js
+++ webapp/playwright.config.js
@@ -5,8 +5,8 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   webServer: {
-    command: "pnpm run dev",
-    url: "http://localhost:3000",
+    command: "pnpm run dev -- --port 3001",
+    url: "http://localhost:3001",
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3001",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
